5a1d626de00d7b9712fd605908d646df20a1586f
Brian Manchester <brian.l.manchester@gmail.com>
2019-02-16 18:40:01 -0800

    One more update

--- app/components/children/Nav.js
+++ app/components/children/Nav.js
@@ -45,7 +45,6 @@ class Nav extends Component {
               <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/portfolio">Apps</Link></li>
-                <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
               </ul>
             </div>
