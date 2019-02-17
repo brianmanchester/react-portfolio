714ba78e9b0b1cc00b7bc6ae1585a90c0de1a00e
Brian Manchester <brian.l.manchester@gmail.com>
2019-02-16 18:38:53 -0800

    Remove about page

--- app/components/children/Nav.js
+++ app/components/children/Nav.js
@@ -27,7 +27,6 @@ class Nav extends Component {
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                   <li><Link to="/">Home</Link></li>
                   <li><Link to="/portfolio">Apps</Link></li>
-                  <li><Link to="/about">About</Link></li>
                   <li><Link to="/contact">Contact</Link></li>
                 </ul>
               </div>
